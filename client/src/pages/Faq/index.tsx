df9b8a09e7dc7c7fd9737222331ee73907aca8b7
jglydes <jonathan_gludo@yahoo.com>
2023-02-06 16:05:34 +0800

    faq sidebar menu

--- client/src/pages/Faq/index.tsx
+++ client/src/pages/Faq/index.tsx
@@ -39,27 +39,45 @@ function Faq()  {
     )
   }
 
+  const Sidebar = () => {
+    return (
+      <div className="sidebar">
+        <p 
+          className={ showFAQ === "TRANSACTION" ? "selectedFaq pointer" : "text-white pointer"}
+          onClick={() => handleSectionShow("TRANSACTION")}
+        >
+          Adjusting Transaction fee in Woodcoin QT wallet
+        </p>
+
+        <p 
+          className={ showFAQ === "METAMASK" ? "selectedFaq pointer" : "text-white pointer"}
+          onClick={() => handleSectionShow("METAMASK")}
+        >
+          Setting up Metamask
+        </p>
+      </div>
+    )
+  }
+
   const FaqSectionFee = () => {
     return (
       <div className="dc mb4" id="transactionRef">
-        <div className={process.env.REACT_APP_THEME === "PURPLE" ? "purple_wood_section" : "dark_green_section"}>
-            <div className="width70 d-flex flex-column justify-content-center align-items-center gp2 text-white padding0Mobile1">
-                <p className="subtitlefs mb4">Adjusting Transaction fee in Woodcoin QT wallet</p>
-                <div className="width70 flex-start-css mv2h1">
-                  <p className="text-justify">In cryptocurrency, the transaction speed depends on the amount of fee you pay. You can reduce transaction time by paying a higher fee.</p>
-                  <p className="text-justify">When using the Woodcoin QT wallet to transfer LOG, we recommend increasing the transaction fee to 0.001 LOG. Doing this can reduce the transaction time to 2-3 minutes.</p>
-                  <p className="text-justify">Follow the instructions below if you want to adjust the transaction fee.</p>
-                </div>
-
-                <p className="text-justify mv2h1">1. In your Woodcoin wallet, click on Settings in the top menu.</p>
-                <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675073771/qt_nav_menu_cdzxfk.png" className="imgSize550" alt="wallet qt nav" />
-
-                <p className="text-justify mv2h1">2. In the dropdown menu, click on Options.</p>
-                <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675073771/qt_nav_settings_sportf.png" className="imgSize550" alt="wallet qt options" />
-
-                <p className="text-justify mv2h1">3. In the Options window, click on the Wallet tab and adjust the Pay transaction fee amount to 0.001 LOG</p>
-                <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675073771/qt_nav_fee_bwuvu3.png" className="imgSize382" alt="wallet qt change fee" />
+        <div className="width70 d-flex flex-column justify-content-center align-items-center gp2 text-white padding0Mobile1">
+            <p className="subtitlefs mb4">Adjusting Transaction fee in Woodcoin QT wallet</p>
+            <div className="width70 flex-start-css mv2h1">
+              <p className="text-justify">In cryptocurrency, the transaction speed depends on the amount of fee you pay. You can reduce transaction time by paying a higher fee.</p>
+              <p className="text-justify">When using the Woodcoin QT wallet to transfer LOG, we recommend increasing the transaction fee to 0.001 LOG. Doing this can reduce the transaction time to 2-3 minutes.</p>
+              <p className="text-justify">Follow the instructions below if you want to adjust the transaction fee.</p>
             </div>
+
+            <p className="text-justify mv2h1">1. In your Woodcoin wallet, click on Settings in the top menu.</p>
+            <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675073771/qt_nav_menu_cdzxfk.png" className="imgSize550" alt="wallet qt nav" />
+
+            <p className="text-justify mv2h1">2. In the dropdown menu, click on Options.</p>
+            <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675073771/qt_nav_settings_sportf.png" className="imgSize550" alt="wallet qt options" />
+
+            <p className="text-justify mv2h1">3. In the Options window, click on the Wallet tab and adjust the Pay transaction fee amount to 0.001 LOG</p>
+            <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675073771/qt_nav_fee_bwuvu3.png" className="imgSize382" alt="wallet qt change fee" />
         </div>
       </div>
     )
@@ -68,41 +86,40 @@ function Faq()  {
   const FaqSectionMetamask = () => {
     return (
       <div className="dc mb4" id="Metamask">
-        <div className={process.env.REACT_APP_THEME === "PURPLE" ? "purple_wood_section" : "dark_green_section"}>
-            <div className="width70 d-flex flex-column justify-content-center align-items-center gp2 text-white padding0Mobile1">
-                <p className="subtitlefs mb4">Using Redeem functionality on Metamask wallet App</p>
+        <div className="width70 d-flex flex-column justify-content-center align-items-center gp2 text-white padding0Mobile1">
+            <p className="subtitlefs mb4">Using Redeem functionality on Metamask wallet App</p>
 
-                <p className="text-justify-bword subtitlefs">Add Polygon network</p>
-                <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675417186/Polygon_network_so2s7y.gif" className="imgSize320" alt="wallet qt nav" />
-                <div className="width70 flex-start-css mv2h1">
-                  <p className="text-justify">- Tap on the three horizontal bars in the upper left-hand corner</p>
-                  <p className="text-justify">- Tap on Settings</p>
-                  <p className="text-justify">- Tap on Networks</p>
-                  <p className="text-justify">- If Polygon Mainnet is not yet available in the network list, tap on Add Network</p>
-                  <p className="text-justify">- Tap on Polygon Mainnet</p>
-                  <p className="text-justify">- Tap on Approve and then "Switch to network"</p>
-                </div>
-
-                <p className="text-justify-bword subtitlefs">Import Wlog Token</p>
-                <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675417186/import_tokens_ajkiku.gif" className="imgSize320" alt="wallet qt nav" />
-                <div className="width70 flex-start-css mv2h1">
-                  <p className="text-justify">- Tap Import Tokens</p>
-                  <p className="text-justify">- Tap the Custom Token tab</p>
-                  <p className="text-justify">- Paste the WLOG contract address to the Token Address input field (0x89F8bE64Da35308260BA2d13d0d1e7Fd80A3A210)</p>
-                  <p className="text-justify">- Tap the IMPORT button, and WLOG will be available in your wallet</p>
-                </div>
-
-                <p className="text-justify-bword subtitlefs">Using metamask browser to access wlog.network</p>
-                <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675417186/WLog_network_ytp5go.gif" className="imgSize320" alt="wallet qt nav" />
-                <div className="width70 flex-start-css mv2h1">
-                  <p className="text-justify">- Tap on the three horizontal bars in the upper left-hand corner</p>
-                  <p className="text-justify">- Tap on Browser</p>
-                  <p className="text-justify">- Open a new tab using the search bar, or click the magnifying glass icon, and navigate to the wlog.network website </p>
-                </div>
-
+            <p className="text-justify-bword subtitlefs">Add Polygon network</p>
+            <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675417186/Polygon_network_so2s7y.gif" className="imgSize320" alt="wallet qt nav" />
+            <div className="width70 flex-start-css mv2h1">
+              <p className="text-justify">- Tap on the three horizontal bars in the upper left-hand corner</p>
+              <p className="text-justify">- Tap on Settings</p>
+              <p className="text-justify">- Tap on Networks</p>
+              <p className="text-justify">- If Polygon Mainnet is not yet available in the network list, tap on Add Network</p>
+              <p className="text-justify">- Tap on Polygon Mainnet</p>
+              <p className="text-justify">- Tap on Approve and then "Switch to network"</p>
             </div>
+
+            <p className="text-justify-bword subtitlefs">Import Wlog Token</p>
+            <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675417186/import_tokens_ajkiku.gif" className="imgSize320" alt="wallet qt nav" />
+            <div className="width70 flex-start-css mv2h1">
+              <p className="text-justify">- Tap Import Tokens</p>
+              <p className="text-justify">- Tap the Custom Token tab</p>
+              <p className="text-justify">- Paste the WLOG contract address to the Token Address input field (0x89F8bE64Da35308260BA2d13d0d1e7Fd80A3A210)</p>
+              <p className="text-justify">- Tap the IMPORT button, and WLOG will be available in your wallet</p>
+            </div>
+
+            <p className="text-justify-bword subtitlefs">Using metamask browser to access wlog.network</p>
+            <img src="https://res.cloudinary.com/dba8ifej6/image/upload/v1675417186/WLog_network_ytp5go.gif" className="imgSize320" alt="wallet qt nav" />
+            <div className="width70 flex-start-css mv2h1">
+              <p className="text-justify">- Tap on the three horizontal bars in the upper left-hand corner</p>
+              <p className="text-justify">- Tap on Browser</p>
+              <p className="text-justify">- Open a new tab using the search bar, or click the magnifying glass icon, and navigate to the wlog.network website </p>
+            </div>
+
         </div>
-      </div>
+    </div>
+      
     )
   }
 
@@ -112,9 +129,16 @@ function Faq()  {
         
         {TitleSection()}
 
-        {showFAQ === "TRANSACTION" ? FaqSectionFee() : null}
+        <div className={process.env.REACT_APP_THEME === "PURPLE" ? "faq-wrapper purple_wood_section_bg" : "faq-wrapper dark_green_section_bg"}>
+          {Sidebar()}
 
-        {showFAQ === "METAMASK" ? FaqSectionMetamask() : null}
+          <div className="wlborder width80">
+            {showFAQ === "TRANSACTION" ? FaqSectionFee() : null}
+
+            {showFAQ === "METAMASK" ? FaqSectionMetamask() : null}
+          </div>
+
+        </div>
 
       </div>
       
